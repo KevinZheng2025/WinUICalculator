8420e685779c4301dc70204cb64ad27d04b65dce
KevinZheng2025 <92385610+KevinZheng2025@users.noreply.github.com>
2022-02-11 22:15:55 -0600

    Add project files.

---
+++ WinUICalculator/MainWindow.xaml.cpp
@@ -0,0 +1,66 @@
+﻿#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::WinUICalculator::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    int32_t MainWindow::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    int firstNum, secondNum, result;
+
+    void MainWindow::conversion() {
+        firstNum = stoi(to_string(txtFirst().Text()));
+        secondNum = stoi(to_string(txtSecond().Text()));
+    }
+
+    void MainWindow::output() {
+        lblResult().Text(L"Result: " + to_hstring(result));
+    }
+
+    void MainWindow::add(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+    {
+        conversion();
+        result = firstNum + secondNum;
+        output();
+    }
+    void MainWindow::sub(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+    {
+        conversion();
+        result = firstNum - secondNum;
+        output();
+    }
+    void MainWindow::div(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+    {
+        conversion();
+        result = firstNum / secondNum;
+        output();
+    }
+    void MainWindow::mult(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+    {
+        conversion();
+        result = firstNum * secondNum;
+        output();
+    }
+}
+
---
+++ WinUICalculator/MainWindow.xaml.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "MainWindow.g.h"
+
+namespace winrt::WinUICalculator::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void conversion();
+        void output();
+        void add(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+
+        void sub(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+
+        void div(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+
+        void mult(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+
+    };
+}
+
+namespace winrt::WinUICalculator::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
